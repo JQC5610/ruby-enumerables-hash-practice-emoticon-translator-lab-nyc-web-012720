ff62d42abffe0e8aa64f453b3694fbecb9e87873
Jason Chung <jqc5610@gmail.com>
2020-01-13 03:00:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,16 +4,17 @@ def load_library(file_path)
   emoticons = YAML.load_file(file_path)
 
   new_hash = {
-    'get_meaning' => {}
+    'get_meaning' => {},
     'get_emoticon' => {}
   }
 
-  emoticons.each do |key, value|
+  emoticons.each do |key,value|
     new_hash['get_emoticon'][value[0]] = emoticons[key][1]
     new_hash['get_meaning'][value[1]] = key
-end
+  end
 
-new_hash
+  new_hash
+end
 
 def get_japanese_emoticon(file_path, emoticon)
   result = load_library(file_path)['get_emoticon'][emoticon]
